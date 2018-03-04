67de278a52fab63f80f7afd046af297d066b3eb9
Marine LB <marine.leborgne@live.fr>
2018-03-05 00:42:24 +0100

    :sparkles: main.js > add animation timeline

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 import TweenMax from 'gsap'
+require('./morphing.js');
 
 document.addEventListener('DOMContentLoaded', function () {
-  TweenMax.from('svg', 3, {delay: 0.3, opacity: 0, ease: Power2.easeOut})
-  TweenMax.from('.title__pagetitle', 2, {delay: 0.3, opacity: 0, ease: Power2.easeOut})
-  TweenMax.from('.title__subtitle--left', 0.7, {delay: 1.5, opacity: 0, bottom: '20px', ease: Power2.easeOut})
-  TweenMax.from('.title__subtitle--right', 0.7, {delay: 2, opacity: 0, top: '20px', ease: Power2.easeOut})
-  TweenMax.from('.content__link', 0.5, {delay: 3.5, opacity: 0, top: '5px', ease: Power2.easeOut})
 
-  TweenMax.from('#corner', 0.3, {delay: 1, morphSVG: "#cornermove"})
+  var tlSvg = new TimelineLite();
+  tlSvg.from('svg', 3, {delay: 0.3, opacity: 0, ease: Power2.easeOut})
+  tlSvg.from('#corner', 10, {morphSVG: '#cornermove', yoyo:true, repeat: -1})
+
+  var tl = new TimelineLite();
+  tl.from('.title__pagetitle', 2, {opacity: 0, ease: Power2.easeOut})
+  tl.from('.title__subtitle--left', 0.7, {opacity: 0, bottom: '20px', ease: Power2.easeOut})
+  tl.from('.title__subtitle--right', 0.7, {opacity: 0, top: '20px', ease: Power2.easeOut})
+  tl.from('.content__link', 0.5, {opacity: 0, top: '5px', ease: Power2.easeOut}, '+=1')
 
 })
